d4b5c4fc04a21271c30119fa5f960df1ff699a1c
lemon <wldn9297@naver.com>
2021-03-08 17:37:55 +0900

    removed hello. java and added menumanager .java

---
+++ src/MenuManger.java
@@ -0,0 +1,12 @@
+
+public class MenuManger {
+
+	public static void main(String[] args) {
+		System.out.println("1.Add Students");
+		System.out.println("2. Delete Students");
+		System.out.println("3. Edit Students");
+		System.out.println("4. view Student");
+		System.out.println("5. Exit");
+	}
+
+}
